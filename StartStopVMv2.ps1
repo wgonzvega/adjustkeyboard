f42ea16df6e9625743a8b3ed41d439f2e0ded01b
Walter Gonzalez <wgonzvega@gmail.com>
2021-06-03 00:19:43 -0400

    beta5

--- StartStopVMv2.ps1
+++ StartStopVMv2.ps1
@@ -38,11 +38,11 @@ $sub1 = "6cfeca43-6518-489d-9a83-f0a9bb0cba5b"
 $subname = ""
 
 Clear-Host 
-    <#Clear-AzContext
+    Clear-AzContext
     Connect-AzAccount #-WarningAction:SilentlyContinue
     $seltenant = Get-AzTenant -TenantId $tenant1  
     write-host "Selected tenant:"$seltenant.name
-    #>
+    
 
 #Set the subscription
 write-host "Tenant Id:" $tenant1
@@ -67,22 +67,19 @@ switch ($listrec){
 
 
 #setvmname1
-$selvm1 = Read-Host "`nPlease enter the Virtual Machine name that you want to work with (Ctrl/C to cancel)"
+$selvm1 = Read-Host "`nPlease enter the name of the Virtual Machine that you want to work with (Ctrl/C to cancel)"
 
 #setvmname2
 clear-host
-write-host "Please confirm your selection by re-entering the server name."
-$selvm2 = Read-Host "`nPlease enter the Virtual Machine name that you want to work with (Ctrl/C to cancel)"
+write-host "`nPlease confirm your selection by re-entering the server name."
+$selvm2 = Read-Host "Please enter one more time the name of the Virtual Machine that you want to work with (Ctrl/C to cancel)"
 
 
 #check if they are equals
 $selvm1 = $selvm1.ToLower()
 #$selrg1 = $selrg1.ToLower()
-
 $selvm2 = $selvm2.ToLower()
 #$selrg2 = $selrg2.ToLower()
-
-
 if ($selvm1 -eq $selvm2){
     $equalname = "y"
 }else{
@@ -90,18 +87,15 @@ if ($selvm1 -eq $selvm2){
     $equalname = "n"
     ;break
 }
+write-host "`nLooking for server at each Resource Group:"
 
+#get the vm and the resource group and compare them
+#look for the vm at each RG
 
-write-host "`nLooking for server at each Resource Group"
-
-#get the vm and the real resource group, compare them
-<#
- look for the vm at each RG
-#>
 $allRG = Get-AzResourceGroup
 $foundit = 0
 foreach ($rec in $allRG){
-    write-host "Checking for VM" $selvm1 "at" $rec.ResourceGroupName
+    write-host "Looking for VM" $selvm1 "at each resource group, please wait..."
     $vmrec = Get-AzVM -ResourceGroupName $rec.ResourceGroupName -Name $selvm1 -ErrorAction:SilentlyContinue
     if($vmrec.name -eq $selvm1){
         $foundit = "1"
@@ -111,20 +105,24 @@ foreach ($rec in $allRG){
 }
 if($foundit -eq "1") {
     write-host "Server" $selvm1 "found at" $foundrg
-    write-host "`nYou have selected..."
+    write-host "`nYou have selected:"
     write-host "Virtual machine   : " $selvm1
     write-host "From subscription : " $subname.Name
     write-host "In the tenant     : " $seltenant.Name
-    $actionrec = Read-Host "`nPlease enter the number of the action that you want to perform (1 = Stop(Deallocate), 2= Start, Ctrl/C to cancel)"
+    $actionrec = Read-Host "`nPlease enter the number of the action that you want to perform (1 = Stop(Deallocate), 2 = Start, Ctrl/C to cancel)"
     switch ($actionrec) {
         "1" {
-            write-host "`nStopping server", $selvm1 , "from Resource Group" $foundrg, "this could take several minutes, please confirm..."
-            Stop-AzVM -ResourceGroupName $foundrg -Name $selvm1
+            write-host "`nStopping server", $selvm1 , "from Resource Group" $foundrg, "this could take several minutes to complete, please confirm..."
+            $result = Stop-AzVM -ResourceGroupName $foundrg -Name $selvm1
+            if ($result.Status -eq "Succeeded"){write-host "`nOperation Id:" $result.OperationId "`nStatus:" $result.Status
+                write-host "`nServer has accepted the stop(deallocate) command.`nPlease wait a few minutes for the Azure portal to update server status."}
             ; break
         }
         "2" {
-            write-host "`nStarting server", $selvm1 , "from Resource Group" $foundrg, "this could take several minutes, please confirm..."
-            Start-AzVM -ResourceGroupName $foundrg -Name $selvm1 -Confirm
+            write-host "`nStarting server", $selvm1 , "from Resource Group" $foundrg, "this could take several minutes to complete, please confirm..."
+            $result = Start-AzVM -ResourceGroupName $foundrg -Name $selvm1 -Confirm
+            if ($result.Status -eq "Succeeded"){write-host "`nOperation Id:" $result.OperationId "`nStatus:" $result.Status 
+                write-host "`nServer has accepted the start command.`nPlease wait a few minutes for the Azure portal to update server status."}
             ; break
         }
         default { "`nSEntered value is out of range, error in action value"; break }
